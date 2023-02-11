a2028ccf1c05685d44f27b4c59792db95ca9733a
k3ssdev <albertoperezdelrio@gmail.com>
2023-02-11 12:18:12 +0100

    change xsd

--- src/Actividad_01/Traspaso.java
+++ src/Actividad_01/Traspaso.java
@@ -1,52 +1,77 @@
 package Actividad_01;
+import Actividad_01.generated.Modulos.*;
+import Actividad_01.generated.Modulos.Modulo.Alumno;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-
-
-// Ejemplo para mostrar por pantalla el contenido de un fichero que contiene texto en formato utf-8
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Traspaso {
+  private ArrayList<Modulo> modulos = new ArrayList<Modulo>();
+  private ArrayList<Alumno> alumnos = new ArrayList<Alumno>();
 
-    public static void main(String[] arg) {
-        File archivo = null;
-        InputStreamReader fr = null;
-        BufferedReader br = null;
+  public void leerDatos() {
+    try {
+      BufferedReader reader = new BufferedReader(new FileReader("notas.txt"));
+      String linea = reader.readLine();
+      while (linea != null) {
+        if (linea.startsWith("@")) {
+          String[] partes = linea.split(" ");
+          String fecha = partes[0].substring(1);
+          String modulo = partes[1];
+          String nombreAlumno = partes[2];
 
-        //Cadena con el directorio actual
-        String currentDir = System.getProperty("user.dir");
+          Alumno alumno = buscarAlumno(nombreAlumno);
+          if (alumno == null) {
+            alumno = new Alumno();
+            alumno.setNombre(nombreAlumno);
+            alumnos.add(alumno);
+          }
 
-        try {
-            // Apertura del fichero y creacion de BufferedReader para poder
-            // hacer una lectura comoda (disponer del metodo readLine()).
-            archivo = new File(currentDir + "/src/Actividad_01/notas.txt");
-            // Inicializamos el lector de ficheros con el formato utf-8, usando el constructor de InputStreamReader que recibe un FileInputStream
-            fr = new InputStreamReader(new FileInputStream(archivo), "UTF-8");
-            // Inicializamos el buffer de lectura con el lector de ficheros
-            br = new BufferedReader(fr);
-        
-            
-            // Estrucutra de control para leer el fichero linea a linea usando el metodo readLine()
-            String linea;
-            while ((linea = br.readLine()) != null) {
-                System.out.println(linea);
-            }
+          Modulo mod = buscarModulo(modulo, fecha);
+          if (mod == null) {
+            mod = new Modulo();
+            mod.setNombre(modulo);
+            mod.setFecha(fecha);
+            modulos.add(mod);
+          }
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-         // En el finally cerramos el fichero, para asegurarnos
-            // que se cierra tanto si todo va bien como si salta 
-            // una excepcion.
-            try {
-                if (null != fr) {
-                    fr.close();
-                }
-            } catch (Exception e2) {
-                e2.printStackTrace();
-            }
+          linea = reader.readLine();
+          while (linea != null && !linea.startsWith("@")) {
+            partes = linea.split("=");
+            String nombreNota = partes[0];
+            double valor = Double.parseDouble(partes[1]);
+
+            Nota nota = new Nota(nombreNota, valor);
+            mod.agregarNota(nota);
+            alumno.agregarNota(nota);
+
+            linea = reader.readLine();
+          }
         }
+      }
+      reader.close();
+    } catch (IOException ex) {
+      ex.printStackTrace();
     }
+  }
+
+  private Alumno buscarAlumno(String nombre) {
+    for (Alumno alumno : alumnos) {
+      if (alumno.getNombre().equals(nombre)) {
+        return alumno;
+      }
+    }
+    return null;
+  }
+
+  private Modulo buscarModulo(String nombre, String fecha) {
+    for (Modulo modulo : modulos) {
+      if (modulo.getNombre().equals(nombre) && modulo.getFecha().equals(fecha)) {
+        return modulo;
+      }
+    }
+    return null;
+  }
 }
