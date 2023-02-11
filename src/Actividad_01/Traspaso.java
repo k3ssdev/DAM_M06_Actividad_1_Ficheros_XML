70d565d4f251d5a827e422590c84fa4e20c412ab
k3ssdev <albertoperezdelrio@gmail.com>
2023-02-11 13:55:55 +0100

    remove unused imports

--- src/Actividad_01/Traspaso.java
+++ src/Actividad_01/Traspaso.java
@@ -6,9 +6,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 
 
 public class Traspaso  {
