14dc3625774938b9fb80764bbd404f8bd725d5c6
k3ssdev <albertoperezdelrio@gmail.com>
2023-02-12 18:23:27 +0100

    update

--- src/Actividad_01/Buscador.java
+++ src/Actividad_01/Buscador.java
@@ -15,7 +15,6 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 
-
 // Clase Buscador
 public class Buscador {
     // Atributos de la clase
@@ -29,54 +28,13 @@ public class Buscador {
         Buscador buscador = new Buscador();
         // Abrir fichero XML y crear DOM
         buscador.abrir_file_DOM();
-        String busqueda = "/modulos/modulo/alumno[nombre='Steve Rogers']";
+        String busqueda = "/modulos/modulo/alumno[nombre='Bruce Banner']";
         // Ejecutar consulta XPath
-        NodeList result = buscador.Ejecutar_XPath(busqueda); 
-        // Mostrar resultado, si lo hay
-        if (result != null) {
-            System.out.print("\033[H\033[2J");           
-            System.out.println("Busqueda: " + busqueda);
-            System.out.println("");
-            System.out.println(" __________________________________________________________");
-            System.out.println("|      Nombre          |  Modulo   |  UF1  |  UF2  |  UF3  |");
-            System.out.println("|----------------------|-----------|-------|-------|-------|");
-            
-            // Recorrer el NodeList y mostrar los resultados
-            for (int i = 0; i < result.getLength(); i++) {
-                // Obtener el nodo actual
-                Node node = result.item(i);
-                // Si el nodo es un elemento (no un texto)
-                if (node.getNodeType() == Node.ELEMENT_NODE) {
-                    // Obtener el elemento
-                    Element element = (Element) node;
-                    
-                    // Obtener los datos del elemento y mostrarlos
-                    String nombre = element.getElementsByTagName("nombre").item(0).getTextContent();
+        NodeList result = buscador.Ejecutar_XPath(busqueda);
 
-                    // Obtener el valor del atributo m del elemento padre
-                    String m = element.getParentNode().getAttributes().getNamedItem("m").getNodeValue();
+        // Mostrar resultado de la consulta
+        buscador.mostrar_resultado(result, busqueda);
 
-                    // Formatear las notas a 2 decimales
-                    String UF1 = String.format("%.2f", Float.parseFloat(element.getElementsByTagName("UF1").item(0).getTextContent()));
-                    String UF2 = String.format("%.2f", Float.parseFloat(element.getElementsByTagName("UF2").item(0).getTextContent()));
-                    String UF3 = String.format("%.2f", Float.parseFloat(element.getElementsByTagName("UF3").item(0).getTextContent()));
-                    // Mostrar los datos
-                    System.out.printf("| %-20s | %-9s | %-5s | %-5s | %-5s |%n", nombre, m, UF1, UF2, UF3);
-                }
-            }
-            System.out.println("|______________________|___________|_______|_______|_______|");
-            System.out.println("");
-            System.out.println("Pulse una tecla para continuar...");
-        
-            // Esperar a que se pulse una tecla
-            try {
-                System.in.read();
-            } catch (Exception e) {
-            }
-        // Mensaje de error si no se encuentra el alumno
-        } else {
-            System.out.println("No se encontró el alumno con nombre 'Steve Rogers'");
-        }
     }
 
     // Método para abrir el fichero XML y crear el DOM
@@ -101,7 +59,7 @@ public class Buscador {
     // Método para ejecutar una consulta XPath
     public NodeList Ejecutar_XPath(String txtconsulta) {
         try {
-            String salida="";
+            String salida = "";
             // Crear objeto XPathExpression y ejecutar la consulta
             exp = xpath.compile(txtconsulta);
             // Obtener el resultado de la consulta
@@ -109,12 +67,65 @@ public class Buscador {
             // Devolver el resultado
             NodeList nodeList = (NodeList) result;
             for (int i = 0; i < nodeList.getLength(); i++) {
-                salida= salida + "\n" + nodeList.item(i).getChildNodes().item(0).getNodeValue();
+                salida = salida + "\n" + nodeList.item(i).getChildNodes().item(0).getNodeValue();
             }
             return nodeList;
         } catch (Exception ex) {
             System.out.println("Error: " + ex.toString());
             return null;
+        }
+    }
+
+    // Método para mostrar el resultado de la consulta
+
+    public void mostrar_resultado(NodeList result, String busqueda) {
+        // Mostrar resultado, si lo hay
+        if (result != null) {
+            System.out.print("\033[H\033[2J");
+            System.out.println("Busqueda: " + busqueda);
+            System.out.println("");
+            System.out.println(" __________________________________________________________");
+            System.out.println("|      Nombre          |  Modulo   |  UF1  |  UF2  |  UF3  |");
+            System.out.println("|----------------------|-----------|-------|-------|-------|");
+
+            // Recorrer el NodeList y mostrar los resultados
+            for (int i = 0; i < result.getLength(); i++) {
+                // Obtener el nodo actual
+                Node node = result.item(i);
+                // Si el nodo es un elemento (no un texto)
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    // Obtener el elemento
+                    Element element = (Element) node;
+
+                    // Obtener los datos del elemento y mostrarlos
+                    String nombre = element.getElementsByTagName("nombre").item(0).getTextContent();
+
+                    // Obtener el valor del atributo m del elemento padre
+                    String m = element.getParentNode().getAttributes().getNamedItem("m").getNodeValue();
+
+                    // Formatear las notas a 2 decimales
+                    String UF1 = String.format("%.2f",
+                            Float.parseFloat(element.getElementsByTagName("UF1").item(0).getTextContent()));
+                    String UF2 = String.format("%.2f",
+                            Float.parseFloat(element.getElementsByTagName("UF2").item(0).getTextContent()));
+                    String UF3 = String.format("%.2f",
+                            Float.parseFloat(element.getElementsByTagName("UF3").item(0).getTextContent()));
+                    // Mostrar los datos
+                    System.out.printf("| %-20s | %-9s | %-5s | %-5s | %-5s |%n", nombre, m, UF1, UF2, UF3);
+                }
+            }
+            System.out.println("|______________________|___________|_______|_______|_______|");
+            System.out.println("");
+            System.out.println("Pulse una tecla para continuar...");
+
+            // Esperar a que se pulse una tecla
+            try {
+                System.in.read();
+            } catch (Exception e) {
+            }
+            // Mensaje de error si no se encuentra el alumno
+        } else {
+            System.out.println("No se encontró el alumno con nombre 'Steve Rogers'");
         }
     }
 }
